be3126e73715ace7f277e89a82a21b920cec866b
sumondebnath <debnathsumon1708@gmail.com>
2024-08-27 21:57:52 +0600

    staticfiles added

--- Transaction_History/settings.py
+++ Transaction_History/settings.py
@@ -159,8 +159,8 @@ MEDIA_ROOT=BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
